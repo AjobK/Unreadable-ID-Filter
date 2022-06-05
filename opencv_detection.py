de7ca223881f55745acb571084930e6da704ddf4
AjobK <ajobkustra.p@gmail.com>
2022-06-05 15:55:32 +0200

    Dirty code, whatever. Included normalization of data and combo choice logic

--- opencv_detection.py
+++ opencv_detection.py
@@ -1,6 +1,8 @@
 # Built on top of Adrian Rosebrock work from pyimagesearch.
 
 from cv2 import threshold
+from skimage.filters import edges
+from skimage import io, color
 from imutils import paths
 import numpy as np
 import argparse
@@ -11,7 +13,8 @@ import time
 path = "data/temp"
 
 def variance_of_laplacian(image):
-    return cv2.Laplacian(image, cv2.CV_64F).var()
+    theVar = cv2.Laplacian(image, cv2.CV_64F).var()
+    return theVar
 
 def detect_blur_fft(image, size=60, thresh=10):
 	(h, w) = image.shape
@@ -29,57 +32,91 @@ def detect_blur_fft(image, size=60, thresh=10):
 
 	return (mean, mean <= thresh)
 
+def sci_variance_of_laplacian(image):
+    theVar = edges.laplace(image, 3).var()
+    return theVar
+
+def sci_detect_blur_fft(image):
+    return cv2.Laplacian(image, cv2.CV_64F).var()
+
+def normalizeData(data):
+    newData = (data - np.min(data)) / (np.max(data) - np.min(data)) * 100
+    print(newData)
+    return newData
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--images", required=True,
     help="path to input directory of images")
 ap.add_argument("-t", "--threshold", type=float, default=100.0,
     help="focus measures that fall below this value will be considered 'blurry'")
+ap.add_argument("-c", "--combination", default="ocv-log",
+    help="The framework and algorithm combination used")
 args = vars(ap.parse_args())
 
 results={
     "sharp": {
-        "positive": 0,
-        "negative": 0,
+        "true": 0,
+        "false": 0,
         "totalTimeMs": 0,
         "avgTimeMs": 0
     },
     "motblurred-real": {
-        "positive": 0,
-        "negative": 0,
-        "totalTimeMs": 0,
-        "avgTimeMs": 0
-    },
-    "defblurred-real": {
-        "positive": 0,
-        "negative": 0,
+        "true": 0,
+        "false": 0,
         "totalTimeMs": 0,
         "avgTimeMs": 0
     },
     "motblurred-synth": {
-        "positive": 0,
-        "negative": 0,
+        "true": 0,
+        "false": 0,
+        "totalTimeMs": 0,
+        "avgTimeMs": 0
+    },
+    "defblurred-real": {
+        "true": 0,
+        "false": 0,
         "totalTimeMs": 0,
         "avgTimeMs": 0
     },
     "defblurred-synth": {
-        "positive": 0,
-        "negative": 0,
+        "true": 0,
+        "false": 0,
         "totalTimeMs": 0,
         "avgTimeMs": 0
     }
 }
 
+lowestFm = [9999, "none"]
+highestFm = [-9999, "none"]
+
 for imagePath in paths.list_images(args["images"]):
     image = cv2.imread(imagePath)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
+    gray = normalizeData(gray)
+
     startTime = time.time()
-    # LoG
+
     fm = variance_of_laplacian(gray)
-    # FFT
-    # (fm, res) = detect_blur_fft(gray)
+
+    if args["combination"].startswith("ocv-"):
+        if args["combination"].endswith("log"):
+            fm = variance_of_laplacian(gray)
+        elif (args["combination"].endswith("fft")):
+            (fm, res) = detect_blur_fft(gray)
+
+    elif args["combination"].startswith("sci-"):
+        if args["combination"].endswith("log"):
+            fm = sci_variance_of_laplacian(gray)
+        elif (args["combination"].endswith("fft")):
+            (fm, res) = sci_detect_blur_fft(gray)
 
     isBlurry = False
+
+    if fm < lowestFm[0]:
+        lowestFm = [fm, imagePath]
+    elif fm > highestFm[0]:
+        highestFm = [fm, imagePath]
+
 
     if fm < args["threshold"]:
         isBlurry = True
@@ -90,32 +127,43 @@ for imagePath in paths.list_images(args["images"]):
         imageBlurType = "%s-%s" % (imageBlurType, "real" if "real" in imagePath else "synth")
 
         if isBlurry:
-            results[imageBlurType]["positive"] = results[imageBlurType]["positive"] + 1
+            results[imageBlurType]["true"] = results[imageBlurType]["true"] + 1
         else:
-            results[imageBlurType]["negative"] = results[imageBlurType]["negative"] + 1
+            results[imageBlurType]["false"] = results[imageBlurType]["false"] + 1
     else:
         if not isBlurry:
-            results["sharp"]["positive"] = results["sharp"]["positive"] + 1
+            results["sharp"]["true"] = results["sharp"]["true"] + 1
         else:
-            results["sharp"]["negative"] = results["sharp"]["negative"] + 1
+            results["sharp"]["false"] = results["sharp"]["false"] + 1
     
     deltaTime = (time.time() - startTime) * 1000
     results[imageBlurType]["totalTimeMs"] = results[imageBlurType]["totalTimeMs"] + deltaTime
-    results[imageBlurType]["avgTimeMs"] = results[imageBlurType]["totalTimeMs"] / (
-        results[imageBlurType]["positive"] + results[imageBlurType]["negative"]
-    )
+    results[imageBlurType]["avgTimeMs"] = round(results[imageBlurType]["totalTimeMs"] / (
+        results[imageBlurType]["true"] + results[imageBlurType]["false"]
+    ), 2)
 
     # For debugging
     # print(imagePath, " - {}: {:.2f}".format("Blurry" if isBlurry else "Not Blurry", fm))
 
-allpositive = results["sharp"]["positive"] + results["motblurred-real"]["positive"] + results["defblurred-real"]["positive"] + results["motblurred-synth"]["positive"] + results["defblurred-synth"]["positive"]
-allFalse = results["sharp"]["negative"] + results["motblurred-real"]["negative"] + results["defblurred-real"]["negative"] + results["motblurred-synth"]["negative"] + results["defblurred-synth"]["negative"]
+truePositives = results["sharp"]["true"]
+trueNegatives = results["motblurred-real"]["true"] + results["defblurred-real"]["true"] + results["motblurred-synth"]["true"] + results["defblurred-synth"]["true"]
+
+falsePositives = results["sharp"]["false"]
+falseNegatives = results["motblurred-real"]["false"] + results["defblurred-real"]["false"] + results["motblurred-synth"]["false"] + results["defblurred-synth"]["false"]
+
+allPositive = trueNegatives + truePositives
+allFalse = falsePositives + falseNegatives
 
 totalResults = {
-    "accuracy": allpositive / (allpositive + allFalse),
-    "recall": results["sharp"]["positive"] / (results["sharp"]["positive"] + results["sharp"]["negative"]),
+    "combo": args["combination"],
+    "accuracy": round(allPositive / (allPositive + allFalse) * 100, 2),
+    "recall": round(truePositives / (truePositives + falsePositives) * 100, 2),
+    "truthMatrix": [truePositives, trueNegatives, falsePositives, falseNegatives],
     "types": results
 }
 
 print("\nTOTAL RESULTS (Threshold = %s)" % args["threshold"])
 print(json.dumps(totalResults, sort_keys=False, indent=4))
+
+print(lowestFm)
+print(highestFm)
